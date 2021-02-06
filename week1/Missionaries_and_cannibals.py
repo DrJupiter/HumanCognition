560ec63602f767ebf42c1d9baa48489fe8aafca3
DrJupiter <klausjupiter@hotmail.com>
2021-02-06 14:30:05 +0100

    Move and ParseMove

---
+++ week1/Missionaries_and_cannibals.py
@@ -0,0 +1,43 @@
+from enum import Enum, unique, auto
+
+
+@unique
+class Move(Enum):
+    AddCannibal = auto(),
+    RemoveCannibal = auto(),
+    AddMissionary = auto(),
+    RemoveMissionary = auto(),
+    MoveBoat = auto(),
+    Invalid = auto(),
+
+def parse_move(string: str) -> Move:
+    string = string.strip()
+    if string == "c":
+       return Move.AddCannibal
+    elif string == "C":
+        return Move.RemoveCannibal
+    elif string == "m":
+       return Move.AddMissionary
+    elif string == "M":
+        return Move.RemoveMissionary
+    elif string == "b":
+        return Move.MoveBoat
+    else:
+        # Add a help message for this in the main loop
+        return Move.Invalid
+
+
+def test_parse_move() -> bool:
+    inputs = [' m ', '   M ', 'c', 'C', 'b', '123123123123']
+    expected_moves = [Move.AddMissionary, Move.RemoveMissionary, Move.AddCannibal, Move.RemoveCannibal, Move.MoveBoat, Move.Invalid]
+    for move, e_move in zip(inputs, expected_moves):
+        if parse_move(move) == e_move:
+            continue
+        else:
+            return False
+    return True
+    
+
+print(test_parse_move())
+
+    
