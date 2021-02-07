723acf10d6d5ea3754729cbf8a8396af7af4ad42
DrJupiter <klausjupiter@hotmail.com>
2021-02-07 21:00:55 +0100

    Main loop for pygame

--- week1/graphical.py
+++ week1/graphical.py
@@ -1,4 +1,5 @@
 import os
+from render_py import generate_scene
 from time import perf_counter
 from collections import defaultdict
 
@@ -14,8 +15,7 @@ from pygame.constants import K_LSHIFT, K_RSHIFT, K_LCTRL, K_RCTRL, K_LALT, K_RAL
 
 MODS = [K_LSHIFT, K_RSHIFT, K_LCTRL, K_RCTRL, K_LALT, K_RALT, K_LMETA, K_RMETA] 
 
-pygame.display.init()
-pygame.display.set_mode([700,400], RESIZABLE)
+
 
 def parse_move_py(event) -> Move:
     mods = pygame.key.get_mods()
@@ -40,38 +40,41 @@ def parse_move_py(event) -> Move:
 
         return Move.Invalid
 
-while True:
-    pygame.event.pump()
-    event = pygame.event.wait()
-#    print(event)
-    if event.type == QUIT:
-       break 
-    elif event.type == VIDEORESIZE:
-        w, h = pygame.display.get_surface().get_size()
-        print(w, h)
-        pygame.display.update()
+#while True:
+#    pygame.event.pump()
+#    event = pygame.event.wait()
+##    print(event)
+#    if event.type == QUIT:
+#       break 
+#    elif event.type == VIDEORESIZE:
+#        w, h = pygame.display.get_surface().get_size()
+#        print(w, h)
+#        pygame.display.update()
+#
+#    elif event.type == KEYDOWN:
+#        move = parse_move_py(event) 
+#        print(move)
+#        if move == Move.Quit:
+#            break
 
-    elif event.type == KEYDOWN:
-        move = parse_move_py(event) 
-        print(move)
-        if move == Move.Quit:
-            break
+def main(current_state = np.array([3,3,1]), default_dict = defaultdict(lambda: 0), move_list = np.array([0,0,1]), err=""):
 
-
-
-"""
-def main(current_state = np.array([3,3,1]), default_dict = defaultdict(lambda: 0), move_list = np.array([0,0,1])):
-    
+    generate_scene(current_state, move_list, screen, width, height, txt_config, err) 
     move_list = np.array([0,0,1])
     start_time = perf_counter()
 
     while True:
-        generate_scene(current_state, move_list)
-#        input_move = input("please input move ")
-        pygame.event.pump()
-        event = pygame.event.await()
 
-        if event.type == KEYDOWN:
+        generate_scene(current_state, move_list, screen, width, height, txt_config, err) 
+
+
+        pygame.event.pump()
+        event = pygame.event.wait()
+
+        if event.type == QUIT:
+            exit(0)
+
+        elif event.type == KEYDOWN:
             move = parse_move_py(event)
 
             if move == Move.Boat:
@@ -81,18 +84,18 @@ def main(current_state = np.array([3,3,1]), default_dict = defaultdict(lambda: 0
                     if move_list[0] > 0 or move_list[1] > 0:
                         update_time_dict(default_dict, new_state, start_time, perf_counter())
                         if sum(new_state) == 0:
-                            generate_scene(new_state, np.array([0,0,0]))
+                            generate_scene(new_state, np.array([0,0,1]), screen, width, height, txt_config, err) 
                             print("GG MATE, ez game ez life")
 
                             bar_plot(default_dict)
                             exit(0)
 
-                        main(new_state, default_dict)
+                        main(new_state, default_dict, err=err)
 
                     else:
-                        input("no people in boat (press enter to continue)")        
+                        err = "No people in boat"        
                 else:
-                    input("Cannot move boat due to too many cannibals on one side (press enter to continue)")
+                    err = "This movement results in too many cannibals on one side"
 
 
             elif move == Move.AddCannibal:
@@ -100,39 +103,49 @@ def main(current_state = np.array([3,3,1]), default_dict = defaultdict(lambda: 0
                     if current_state[1] > move_list[1] and sum(move_list) != 3:
                         move_list += [0,1,0]
                     else: 
-                        input("invalid movement of cannibal (press enter to continue)")
+                        err = "Invalid movement of cannibal"
                 else:
                     if 3-current_state[1] > move_list[1] and sum(move_list) != 3:
                         move_list += [0,1,0]
                     else:
-                        input("invalid movement of cannibal (press enter to continue)")
+                        err = "Invalid movement of cannibal"
 
             elif move == Move.RemoveCannibal:
                 if move_list[1] != 0:
                     move_list += [0,-1,0]
                 else:
-                    input("invalid movement of cannibal (press enter to continue)")
+                    err = "Invalid movement of cannibal"
 
             elif move == Move.AddMissionary:
                 if current_state[2] == 1:
                     if current_state[0] > move_list[0] and sum(move_list) != 3:
                         move_list += [1,0,0]
                     else:
-                        input("invalid movement of missionary  (press enter to continue)")
+                        err = "Invalid movement of missionary "
                 else:
                     if 3-current_state[0] > move_list[0] and sum(move_list) != 3:
                         move_list += [1,0,0]
                     else: 
-                        input("invalid movement of missionary  (press enter to continue)")
+                        err = "Invalid movement of missionary "
 
             elif move == Move.RemoveMissionary:
                 if move_list[0] != 0:
                     move_list += [-1,0,0]
                 else:
-                    input("invalid movement of missionary  (press enter to continue)")
+                    err = "Invalid movement of missionary "
 
             elif move == Move.Invalid:
-                input("invalid input (press enter to continue)")
+                err = "Invalid input"
+
+            elif move == Move.Quit:
+                exit(0)
             else:
                 continue
-"""
+
+pygame.display.init()
+width, height = 500, 500
+screen = pygame.display.set_mode([width,height])
+pygame.font.init()
+txt_config = pygame.font.SysFont('Times New Roman', 20)
+
+main()
