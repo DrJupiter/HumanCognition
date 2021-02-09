82c3f878505cc5a4fae1a17eb2ad16c37e2ce356
Skroko <73649109+Skroko@users.noreply.github.com>
2021-02-09 23:20:06 +0100

    Update graphical.py

--- week1/graphical.py
+++ week1/graphical.py
@@ -4,7 +4,7 @@ from time import perf_counter
 from collections import defaultdict
 
 
-from libmissionaries_and_cannibals import valid_state, Move, state_transition, update_time_dict, bar_plot
+from libmissionaries_and_cannibals import valid_state, Move, state_transition, update_time_dict, bar_plot, to_csv
 
 import numpy as np
 
@@ -86,8 +86,9 @@ def main(current_state = np.array([3,3,1]), default_dict = defaultdict(lambda: 0
                             generate_scene(new_state, np.array([0,0,1]), screen, width, height, txt_config, err) 
                             print("GG MATE, ez game ez life")
 
-                            bar_plot(default_dict)
                             to_csv(default_dict)
+                            bar_plot(default_dict)
+                            
                             exit(0)
 
                         main(new_state, default_dict, err="")
