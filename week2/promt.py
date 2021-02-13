16e3dfcd667347b7a7152e43c0c1b85de588f99f
Skroko <73649109+Skroko@users.noreply.github.com>
2021-02-13 15:34:06 +0100

    +1

--- week2/promt.py
+++ week2/promt.py
@@ -5,17 +5,12 @@ def gen_random_matrix(size):
     return np.random.rand(size,size)
 
 
-"""
 for _ in range(1000):
     neuron_a = Neuron('A') #<- INSERT NEURON LETTER HERE 
 
-    neuron_a(gen_random_matrix)  # Outputs scaler indicating how active teh neruon is with respect to its baseline activity
+    neuron_a(gen_random_matrix(100))  # Outputs scaler indicating how active teh neruon is with respect to its baseline activity
                                  # A value of -1 means that the neuron is maximally inhibited. 
                                  # A value of 0 means that the neuron is active at its baseline level. 
                                  # A value of 1 means that the neuron is maximally active.
 
     # INSTERT CODE HERE / change code
-
-
-
-"""
