68f3a2f7aae6cc1e6d68e5dbd589de9437649637
Skroko <73649109+Skroko@users.noreply.github.com>
2021-02-15 10:33:28 +0100

    undo

--- week2/promt.py
+++ week2/promt.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+def gen_random_matrix(dimensions):
+#    return np.random.randint(0, 2, size=dimensions)
+    return np.random.rand(*dimensions)
+
+
 from libneuron import Neuron
 
 # Neurons
@@ -9,8 +14,7 @@ neuron_c = Neuron('C') #<- INSERT NEURON LETTER HERE
 
 for _ in range(10):
 
-    print(neuron_a(np.zeros((100,100)))) # <- INPUT MATRIX HERE
-                                 # Outputs scaler indicating how active the neruon is with respect to its baseline activity
+    print(neuron_a(gen_random_matrix((100,100))))  # Outputs scaler indicating how active the neruon is with respect to its baseline activity
                                  # A value of -1 means that the neuron is maximally inhibited. 
                                  # A value of 0 means that the neuron is active at its baseline level. 
                                  # A value of 1 means that the neuron is maximally active.
