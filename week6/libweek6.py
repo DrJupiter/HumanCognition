1aa241c15d7b14f3ee1b8e758edbb0bb10a1109b
DrJupiter <klausjupiter@hotmail.com>
2021-03-14 15:42:22 +0100

    Week 6 done

--- week6/libweek6.py
+++ week6/libweek6.py
@@ -15,7 +15,17 @@ CODE_PATH = Path(*Path(os.path.realpath(sys.argv[0])).parts[:-1])
 
 def hop_field_net(n_patterns: int = 5,
  test_pattern_noise_lvl: float = 0., wmtrx_noise_lvl: float = 0.):
+    """
+    hop_field_net takes 3 parameters.
 
+    The first n_patterns determines how many patterns are loaded as well as how many iterations are done during training.
+
+    The second test_pattern_noise_lvl determines the frequency at which pixels are flipped at in the initial selected image
+
+    The third wmtrx_noise_lvl determines how much to scale the noise added to the weight matrix by.
+
+    The hops field network is then run, and the outputs of it are displayed.
+    """
     if n_patterns > 15 or n_patterns < 1:
         print(f"The number of patterns must be between 1 and 15, received {n_patterns}")
 
@@ -27,47 +37,52 @@ def hop_field_net(n_patterns: int = 5,
 
 
 
-    train_patterns = load_patterns_rand(n_patterns)
+    train_patterns = load_patterns_rand(n_patterns, "patterns.csv")
 
     n_neurons = train_patterns.shape[1]
 
     w_mtrx = weight_matrix(n_neurons, n_patterns, train_patterns, wmtrx_noise_lvl)
 
-    # a boolean matrix
-    #flip_mtrx = np.where(np.random.rand(n_neurons) < test_pattern_noise_lvl, True, False)
-
     flip_mtrx = np.where(np.random.rand(n_neurons) < test_pattern_noise_lvl)
 
-    test_pattern_idx = 1
-    test_pattern = train_patterns[test_pattern_idx, :]
+    test_pattern_idx = 0
+    test_pattern = deepcopy(train_patterns[test_pattern_idx, :])
     test_pattern[flip_mtrx] = np.random.randint(0,1,size=test_pattern[flip_mtrx].shape)*2-1
 
     iterations = np.zeros(shape=(n_patterns, test_pattern.shape[0]))
 
     iterations[0] = test_pattern
 
-    for i in range(len(iterations)-1):
-        #iterations[i+1] = np.sign(w_mtrx * (iterations[i, :]))
-        #iterations[i+1] = np.sign(w_mtrx * (iterations[i, :]))
-        iterations[i+1] = np.sign(np.dot(w_mtrx , iterations[0, :]))
 
+    for i in range(len(iterations)-1):
+        iterations[i+1] = np.sign(np.dot(w_mtrx , iterations[i, :]))
+
+    show_patterns(train_patterns, [train_patterns[test_pattern_idx]], iterations)
 
 
 
 def weight_matrix(n_neurons, n_patterns, train_patterns, wmtrx_noise_lvl):
+    """
+    Generates Weight_matrix based on [(n_neurons, n_patterns, train_patterns, wmtrx_noise_lvl]
+    """
     w_mtrx = np.zeros(n_neurons)
 
     for i in range(0,n_patterns):
-        w_mtrx = w_mtrx + np.transpose(train_patterns[i,:]) * train_patterns[i,:] - np.identity(n_neurons)
-
+        h = np.transpose(train_patterns[i,:].reshape(1,100)) * train_patterns[i,:].reshape(1,100)
+        w_mtrx = w_mtrx + h - np.identity(n_neurons)
+#        print((np.transpose(train_patterns[i,:].reshape(1,100)) * train_patterns[i,:].reshape(1,100)).shape)
+#        print(train_patterns[i,:].reshape(1,100))
+    
     w_mtrx = w_mtrx/n_patterns
     w_mtrx = w_mtrx + (np.random.rand(n_neurons)-1/2) * wmtrx_noise_lvl
 
     return w_mtrx
 
-def load_patterns():
-
-    with open(CODE_PATH.joinpath("patterns.csv"), newline='') as csv_file:
+def load_patterns(path):
+    """
+    Loads all patterns from csv file which is in the same directory as the rest of the code.
+    """
+    with open(CODE_PATH.joinpath(path), newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         data = []
         for row in csv_reader:
@@ -77,9 +92,11 @@ def load_patterns():
 
     return data
 
-def load_patterns_rand(n_patterns: int):
-
-    patterns = load_patterns() 
+def load_patterns_rand(n_patterns: int, path):
+    """
+    Choses "n_patterns" random patterns from the 15 patterns
+    """
+    patterns = load_patterns(path) 
 
     _pat_range = [i for i in range(0,15)]
     shuffle(_pat_range)
@@ -89,43 +106,37 @@ def load_patterns_rand(n_patterns: int):
 
     return train_patterns
 
-def show_patterns(train_patterns: list, selected_pattern: list, inter_patterns: list):
+def show_patterns(train_patterns: list, selected_pattern: list, iter_patterns: list):
+    """
+    Plots the patterns (Train: top, selected: middle, Iter: bot)
+    """
 
-    fig, axs = plt.subplots(3,len(train_patterns))
-    fig.subplots_adjust(hspace = -.5, wspace=0.2)
+    fig, axs = plt.subplots(3,len(iter_patterns))
+    fig.subplots_adjust(hspace = .5, wspace=0.2)
 
     for i in range(len(train_patterns)):
         axs[0, i].imshow(np.rot90(train_patterns[i].reshape((10,10)),axes=(1,0)), 'gray')
         axs[0, i].get_xaxis().set_visible(False)
         axs[0, i].get_yaxis().set_visible(False)
-        axs[0,i].set_title(f"Pattern {i}")
+        axs[0,i].set_title(f"Pattern {i+1}")
 
     for i in range(len(selected_pattern)):
         axs[1, i].imshow(np.rot90(selected_pattern[i].reshape((10,10)),axes=(1,0)), 'gray')
         axs[1, i].get_xaxis().set_visible(False)
         axs[1, i].get_yaxis().set_visible(False)
-        axs[1,i].set_title(f"Selected Pattern {i}")
+        axs[1,i].set_title(f"Selected Pattern {i+1}")
 
     for i in range(len(selected_pattern), len(train_patterns)):
         axs[1, i].axis('off')
 
-    for i in range(len(inter_patterns)):
-        axs[2, i].imshow(np.rot90(inter_patterns[i].reshape((10,10)),axes=(1,0)), 'gray')
+    for i in range(len(iter_patterns)):
+        axs[2, i].imshow(np.rot90(iter_patterns[i].reshape((10,10)),axes=(1,0)), 'gray')
         axs[2, i].get_xaxis().set_visible(False)
         axs[2, i].get_yaxis().set_visible(False)
-        axs[2,i].set_title(f"Iteration {i}")
+        axs[2,i].set_title(f"Iteration {i+1}")
 
     plt.show()
 
 
-"""
-patts = load_patterns()
-r_patts = []
-for i in range(len(patts)):
-    r_patts.append(patts[i])
-
-show_patterns(r_patts, [r_patts[5]], r_patts)
-"""
-
 if __name__ == "__main__":
-    hop_field_net()
+    hop_field_net(15, 0.9, 0.6)
