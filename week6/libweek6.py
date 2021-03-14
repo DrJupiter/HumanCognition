9259dc29c040031512144e178e548451b7464bb2
Skroko <73649109+Skroko@users.noreply.github.com>
2021-03-14 12:08:39 +0100

    +1

--- week6/libweek6.py
+++ week6/libweek6.py
@@ -1,3 +1,26 @@
 import csv
+import numpy as np
 
-with open('Patterns.csv')
+# For code path
+import sys
+import os
+from pathlib import Path
+
+CODE_PATH = Path(*Path(os.path.realpath(sys.argv[0])).parts[:-1])
+
+
+
+
+with open(CODE_PATH.joinpath("patterns.csv"), newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    data = []
+    for row in csv_reader:
+        data.append(row)
+        
+    data = np.array(data)
+        
+
+print(data)
+print(np.shape(data))
+        
+print(data[1][1])
